73e62e042f0249cef22123d3cde6b23dd99b653f
willwtang <willwyatttang@gmail.com>
2016-05-06 15:48:44 -0700

    Make VideoListEntry tests pass

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,6 +1,6 @@
 var VideoList = (props) => (
   <div className="video-list media">
-    {props.videos.map((video) => (<VideoListEntry />))}
+    {props.videos.map((video) => (<VideoListEntry video={video} />))}
   </div>
 );
 
